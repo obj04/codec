8a49f81ead804260be2ca6cb1b199860ea835374
Tobias Rappsilber <obj04.dev@gmail.com>
2020-03-08 09:53:04 +0100

    Initial commit

---
+++ src/com/github/obj04/codec/CoDec.java
@@ -0,0 +1,15 @@
+package com.github.obj04.codec;
+
+import javax.swing.*;
+
+public class CoDec {
+    static JFrame mainWindow;
+
+    public static void main(String[] args) {
+        mainWindow = new JFrame("CoDec");
+        mainWindow.setSize(640, 480);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mainWindow.setVisible(true);
+    }
+}
